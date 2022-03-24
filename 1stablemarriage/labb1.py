c01f8069e49a64b728a10d9d8715ad4f775f91af
andreaskarlsson <andreasjankarlsson@gmail.com>
2022-03-24 14:05:26 +0100

    read in is done

--- 1stablemarriage/labb1.py
+++ 1stablemarriage/labb1.py
@@ -1,17 +1,27 @@
 
-textFile = open("1stablemarriage/data/sample/1.in", 'r')
+textFile = open("1stablemarriage/data/secret/0testsmall.in", 'r')
 lines = textFile.readlines()
 nbrPairs = int (lines[0])
 
-men_pref = dict()
 wom_pref = dict()
+men_pref = dict()
 
 count=1
 for i in range(1,len(lines)):
     lineData =  lines[i].strip().split()
     lineData = list(map(int, lineData))
-    print(lineData)
-    print(count)
-    count += 1
+    prefList = lineData[1:]
+    if not lineData[0] in wom_pref:
+        wom_pref[lineData[0]]= prefList
+    else:
+        men_pref[lineData[0]]= prefList
 
+print("Wom_pref")
+for i in wom_pref:
+    print('woman' + str(i))
+    print(wom_pref[i])
 
+print("man_pref")
+for i in men_pref:
+    print("man" + str(i))
+    print(men_pref[i])
