abadcfe9c2486b9a16e8cb6e2f6db2052b5b338f
andreaskarlsson <andreasjankarlsson@gmail.com>
2022-03-24 14:59:05 +0100

    updated again

--- 1stablemarriage/labb1.py
+++ 1stablemarriage/labb1.py
@@ -1,27 +1,23 @@
 
-textFile = open("1stablemarriage/data/secret/0testsmall.in", 'r')
+textFile = open("1stablemarriage/data/sample/1.in", 'r')
 lines = textFile.readlines()
 nbrPairs = int (lines[0])
 
 wom_pref = dict()
 men_pref = dict()
+pairs = dict()
+singleMen = list()
 
-count=1
 for i in range(1,len(lines)):
     lineData =  lines[i].strip().split()
     lineData = list(map(int, lineData))
-    prefList = lineData[1:]
+    prefList = dict()
+    for j in range(1, len(lineData[1:])):
+        prefList[j] = j
+
     if not lineData[0] in wom_pref:
         wom_pref[lineData[0]]= prefList
     else:
         men_pref[lineData[0]]= prefList
 
-print("Wom_pref")
-for i in wom_pref:
-    print('woman' + str(i))
-    print(wom_pref[i])
 
-print("man_pref")
-for i in men_pref:
-    print("man" + str(i))
-    print(men_pref[i])
