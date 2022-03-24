4dfde2a2d323c28f3846b6b40a34d46b176889bc
andreaskarlsson <andreasjankarlsson@gmail.com>
2022-03-24 13:43:59 +0100

    updated

--- 1stablemarriage/labb1.py
+++ 1stablemarriage/labb1.py
@@ -1,11 +1,17 @@
 
 textFile = open("1stablemarriage/data/sample/1.in", 'r')
 lines = textFile.readlines()
-print(lines)
-count = 0
-for line in lines:
-    count += 1
-    print("Line{}: {}".format(count, line.strip()))
+nbrPairs = int (lines[0])
 
+men_pref = dict()
+wom_pref = dict()
+
+count=1
+for i in range(1,len(lines)):
+    lineData =  lines[i].strip().split()
+    lineData = list(map(int, lineData))
+    print(lineData)
+    print(count)
+    count += 1
 
 
